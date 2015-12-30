e71de46c885e9788ef9857fcafd33d7d47ad92a2
Martin Camitz <martin.camitz@gmail.com>
2015-12-30 12:58:09 +0100

    Initial commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ticketfpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ticketfpApp
+ */
+angular.module('ticketfpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
